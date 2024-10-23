7408e967e29d1f03ec0a6921f18dabc08b3bfbe0
Daniel Sarney <daniel_sarney@live.co.uk>
2024-10-23 16:47:26 +0100

    Square Drawing Optionalized

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import turtle
 
-timmy_the_turtle = turtle.Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("green4")
+tim = turtle.Turtle()
+tim.shape("turtle")
+tim.color("green4")
 
-def line():
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
-
-
-for n in range(0, 4):
-    line()
+for _ in range(4):
+    tim.forward(100)
+    tim.right(90)
 
 screen = turtle.Screen()
 screen.exitonclick()
