7df172ddda4740c72fe42c29323469c087d6854d
Daniel Sarney <daniel_sarney@live.co.uk>
2024-10-23 16:43:43 +0100

    Square Drawing

--- main.py
+++ main.py
@@ -4,18 +4,14 @@ timmy_the_turtle = turtle.Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("green4")
 
-
-
-
-
-
-
-
-
-
-
-
-
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
 
 
 screen = turtle.Screen()
