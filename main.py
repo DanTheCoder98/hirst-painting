299b9bc59d65657cdaf14e83217405e700c4d0f4
Daniel Sarney <daniel_sarney@live.co.uk>
2024-10-23 16:45:22 +0100

    Square Drawing Function

--- main.py
+++ main.py
@@ -4,15 +4,13 @@ timmy_the_turtle = turtle.Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("green4")
 
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
+def line():
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
 
+
+for n in range(0, 4):
+    line()
 
 screen = turtle.Screen()
 screen.exitonclick()
