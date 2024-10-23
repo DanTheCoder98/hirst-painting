7ac4451bdfb7ccc397c440a3da40d941bb321ff3
Daniel Sarney <daniel_sarney@live.co.uk>
2024-10-23 16:57:34 +0100

    Dashed Line

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ tim = turtle.Turtle()
 tim.shape("turtle")
 tim.color("green4")
 
-for _ in range(4):
-    tim.forward(100)
-    tim.right(90)
+for _ in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+
 
 screen = turtle.Screen()
 screen.exitonclick()
