ecdd930eae130b8ea5956cd1643f6fea55ab9e8a
Daniel Sarney <daniel_sarney@live.co.uk>
2024-10-23 17:11:30 +0100

    Polygon shape

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ tim = turtle.Turtle()
 tim.shape("turtle")
 tim.color("green4")
 
-for _ in range(15):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+def draw_polygon(sides):
+    angle = 360 / sides
+    for _ in range(sides):
+        tim.forward(100)
+        tim.right(angle)
 
+draw_polygon(3)   # Triangle
+draw_polygon(4)   # Square
+draw_polygon(5)   # Pentagon
+draw_polygon(6)   # Hexagon
+draw_polygon(7)   # Heptagon
+draw_polygon(8)   # Octagon
+draw_polygon(9)   # Nonagon
+draw_polygon(10)  # Decagon
 
 screen = turtle.Screen()
 screen.exitonclick()
