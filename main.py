8f89e590fb19195458a3eae2335d4478bb1e02bd
Daniel Sarney <daniel_sarney@live.co.uk>
2024-10-23 18:28:55 +0100

    Colour Palette

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-import turtle
-import random
+import colorgram
 
-tim = turtle.Turtle()
-tim.shape("turtle")
-tim.color("green4")
-tim.speed("fastest")
+image_colours = colorgram.extract("image.jpg", 1000)
 
-screen = turtle.Screen()
-screen.colormode(255)
+colour_palette = []
 
-def random_colour():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
+for n in range(len(image_colours)):
+    rgb_colours = image_colours[n].rgb.r, image_colours[n].rgb.g, image_colours[n].rgb.b
+    colour_palette.append(rgb_colours)
 
-    return (r, g, b)
-
-def draw_spirograph(size_of_gap):
-    for _ in range(int(360 / size_of_gap)):
-        tim.pencolor(random_colour())
-        tim.circle(100)
-        tim.setheading(tim.heading() + size_of_gap)
-
-draw_spirograph(40)
-
-screen.exitonclick()
+print(colour_palette)
