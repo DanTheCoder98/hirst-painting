c4bd81cad1f8f8085a6366dcc08305038d7558cc
Daniel Sarney <daniel_sarney@live.co.uk>
2024-10-23 18:55:58 +0100

    Hide Marker

--- main.py
+++ main.py
@@ -19,16 +19,13 @@ screen.title("Hirst Painting")
 screen.bgcolor("white")
 
 tim = turtle.Turtle()
-tim.shape("circle")
+tim.hideturtle()
 
 def draw_dot(x, y):
     tim.penup()
     tim.goto(x, y)
     tim.pendown()
-    
-    rand_colour = random.choice(colour_palette)
-    tim.color(rand_colour)
-    tim.dot(20, rand_colour)
+    tim.dot(20, random.choice(colour_palette))
 
 start_x = -200
 start_y = -200
@@ -37,5 +34,4 @@ for row in range(10):
     for col in range(10):
         draw_dot(start_x + col * 50, start_y + row * 50)
 
-tim.hideturtle()
 screen.exitonclick()
